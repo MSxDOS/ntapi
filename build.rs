cba954f11d96f673c8b477ab79534f61dcdd0903
Mateusz Mikuła <mati865@gmail.com>
2020-08-13 17:51:40 +0200

    Allow linking to system ntdll

--- build.rs
+++ build.rs
@@ -1,9 +1,15 @@
+use std::env::var;
+
 fn main() {
     #[cfg(feature = "user")] {
         if std::env::var("TARGET").map(
             |t| t == "x86_64-pc-windows-gnu" || t == "i686-pc-windows-gnu"
         ).unwrap_or(false) {
-            println!("cargo:rustc-link-lib=winapi_ntdll");
+            if var("WINAPI_NO_BUNDLED_LIBRARIES").is_ok() {
+                println!("cargo:rustc-link-lib=ntdll");
+            } else {
+                println!("cargo:rustc-link-lib=winapi_ntdll");
+            }
         }
     }
 }
