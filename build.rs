a9155c403fca9b91b0c30dc1161b0b6412656daf
Arghyadip Chakraborty <arghyadip.chak16@gmail.com>
2023-06-05 17:23:08 +0530

    ntdll link fix

--- build.rs
+++ build.rs
@@ -1,13 +1,6 @@
-use std::env::var;
-
 fn main() {
     #[cfg(feature = "user")]
     {
-        if var("TARGET")
-            .map(|t| t == "x86_64-pc-windows-gnu" || t == "i686-pc-windows-gnu")
-            .unwrap_or(false)
-        {
-            println!("cargo:rustc-link-lib=ntdll");
-        }
+        println!("cargo:rustc-link-lib=ntdll");
     }
 }
